171bc44eae7811e1841049c6a29398d0cb07d659
gabrirm <gabrirm00@gmail.com>
2023-09-20 21:35:56 +0200

    implemented fake authentication

--- worldwise/src/pages/Login.jsx
+++ worldwise/src/pages/Login.jsx
@@ -1,16 +1,32 @@
+import { useNavigate } from "react-router-dom";
 import PageNav from "../components/PageNav";
+import { useAuth } from "../contexts/FakeAuthContext";
 import styles from "./Login.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Button from "../components/Button";
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
-
+  const { login, isAuth, user } = useAuth();
+  const navigate = useNavigate();
+  function handleLogin(e) {
+    if (!email || !password) return;
+    e.preventDefault();
+    login(email, password);
+  
+  }
+  useEffect(
+    function () {
+      if (isAuth) navigate("/app", { replace: true });
+    },
+    [isAuth, navigate]
+  );
   return (
     <main className={styles.login}>
-      <PageNav/>
-      <form className={styles.form}>
+      <PageNav />
+      <form className={styles.form} onSubmit={(e) => handleLogin(e)}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
           <input
@@ -32,7 +48,7 @@ export default function Login() {
         </div>
 
         <div>
-          <button>Login</button>
+          <Button type="primary">Login</Button>
         </div>
       </form>
     </main>
---
+++ worldwise/src/pages/ProtectedRoute.jsx
@@ -0,0 +1,18 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/FakeAuthContext";
+import { useEffect } from "react";
+
+function ProtectedRoute({ children }) {
+  const { isAuth } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(
+    function () {
+      if (!isAuth) navigate("/");
+    },
+    [isAuth, navigate]
+  );
+  return isAuth ? children : null
+}
+
+export default ProtectedRoute;
